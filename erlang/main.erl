5c88656bab49bddc865a1d3d6c759707896405a0
Pertii8 <valentinoperticarari8@gmail.com>
2026-07-21 13:00:13 -0300

    Faltaba parametro en manual_loop

--- erlang/main.erl
+++ erlang/main.erl
@@ -76,8 +76,8 @@ server(Modo, N, Puerto, TimeJob, JobTimeoutInit) ->
 % Espera que el usuario mande jobs armados a mano desde la consola, uno por uno, 
 % spawneando un handler_job por cada uno. 
 % Termina al recibir 'fin', momento en el que limpia las tablas ETS usadas durante la ejecución.
-% Recibe: Socket
-manual_loop(Socket) -> %Asi deberia quedar el string a mandar a C  JOB_REQUEST 1001 192.168.1.2:cpu:2 192.168.1.3:gpu:1
+% Recibe: Socket, TimeJob(int)
+manual_loop(Socket, TimeJob) -> %Asi deberia quedar el string a mandar a C  JOB_REQUEST 1001 192.168.1.2:cpu:2 192.168.1.3:gpu:1
     receive     
         %Desde consola envias el JOB entero por ej: {"192.168.1.2:cpu:2 192.168.1.3:gpu:1"}.
         {Job} ->
@@ -86,8 +86,8 @@ manual_loop(Socket) -> %Asi deberia quedar el string a mandar a C  JOB_REQUEST 1
                 Msg_REQUEST = "JOB_REQUEST" ++ " " ++ JobID ++ " " ++ Job,
                 Msg_RELEASE = "JOB_RELEASE" ++ " " ++ JobID,
                 %Pasamos 0 en CantRecusos pq no importan y ademas procesar_respuesta los ignora. 
-                spawn(job_manager, handler_job, [JobID, Job, 0, 3000, Socket, Msg_REQUEST, Msg_RELEASE, []]), %Manda el msg al agente espera su rta y la maneja
-                manual_loop(Socket);
+                spawn(job_manager, handler_job, [JobID, Job, 0, 3000, Socket, Msg_REQUEST, Msg_RELEASE, [], TimeJob]), %Manda el msg al agente espera su rta y la maneja
+                manual_loop(Socket, TimeJob);
         % Terminará cuando el usuario mande cliente_pid ! fin.
         fin ->  ok 
     end,
@@ -119,7 +119,7 @@ client(Modo, N, Puerto, TimeJob, JobTimeoutInit) ->
             ets:delete(recursos_nodos); %Liberamos la tabla de recursos_nodos.
 
         manual ->
-            manual_loop(Socket); % Mismo socket que usa tcp_deliver
+            manual_loop(Socket, TimeJob); % Mismo socket que usa tcp_deliver
 
         _ -> 
             io:format("Los modos son: manual o random~n"),
